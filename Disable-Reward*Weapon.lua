c934f456ac2f464226beb9d84b62826ec8994605
Rzoxs <57816758+Yt-Rzoxs@users.noreply.github.com>
2022-06-30 00:41:45 +0200

    Update Disable-Reward*Weapon.lua

--- Disable-Reward*Weapon.lua
+++ Disable-Reward*Weapon.lua
@@ -4,6 +4,7 @@ AddEventHandler('gameEventTriggered', function (name, args)
             while true do
                 Wait(0)
                 DisablePlayerVehicleRewards(-1)
+                RemoveWeaponFromPed(PlayerPedId(), GetHashKey("WEAPON_SNIPER"))
                 if not IsPedInAnyVehicle(GetPlayerPed(-1), false) then
                     break
                 end
