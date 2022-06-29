8e6d3b11a265c3fad9411998bfa4caf3b62df1b1
Rzoxs <57816758+Yt-Rzoxs@users.noreply.github.com>
2022-06-29 21:46:07 +0200

    Update Disable-Reward*Weapon.lua

--- Disable-Reward*Weapon.lua
+++ Disable-Reward*Weapon.lua
@@ -1,3 +1,14 @@
-Citizen.CreateThread(function()
-    DisablePlayerVehicleRewards(-1)
+AddEventHandler('gameEventTriggered', function (name, args)
+    if name == "CEventNetworkPlayerEnteredVehicle" then
+        Citizen.CreateThread(function()
+            while true do
+                Wait(0)
+                if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
+                    DisablePlayerVehicleRewards(-1)
+                else
+                    break
+                end
+            end
+        end)
+    end
 end)
